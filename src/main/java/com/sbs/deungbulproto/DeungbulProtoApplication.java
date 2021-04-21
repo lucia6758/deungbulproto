f2f26534b3793e9b44ee588f8069bd4ac03b8831
cjy324 <juy32400@naver.com>
2021-04-21 13:03:39 +0900

    21.04.21 일단 백엔드 context는 OK

--- src/main/java/com/sbs/deungbulproto/DeungbulProtoApplication.java
+++ src/main/java/com/sbs/deungbulproto/DeungbulProtoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeungbulProtoApplication {
+public class DeungbulProtoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeungbulProtoApplication.class, args);
-		DeungbulProtoApplication app = new DeungbulProtoApplication();
+//		DeungbulProtoApplication app = new DeungbulProtoApplication();
     }
 }
